7b9185873deae145fd8aeb80ca0bafdb9a5da87e
Aditya72 <adityakumar72@deloitte.com>
2022-03-31 17:58:41 +0530

    Mini_Assignment_2_Added

---
+++ Mini_Assignment_2/src/test/java/put.java
@@ -0,0 +1,35 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+public class put {
+
+    String uri2= "https://reqres.in/api";
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+    @BeforeSuite
+    public void nothing() {
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+    @BeforeTest
+    public void setup(){
+        RequestSpecBuilder requestSpecBuilder= new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri(uri2)
+                .addHeader("Content-Type","application/json");
+        requestSpecification= RestAssured.with().spec(requestSpecBuilder.build());
+        responseSpecification=RestAssured.expect()
+                .contentType(ContentType.JSON);
+    }
+    @Test
+    public void put_call(){
+        Response response=requestSpecification.put("/users");
+        assertThat(response.statusCode(), equalTo(200));
+    }
+}
